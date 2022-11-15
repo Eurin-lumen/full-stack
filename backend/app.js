860c02016397f7c808d1fd79ba5aecb909ce8d4a
Eurin-lumen <eurindelagoldcoast@gmail.com>
2022-11-16 00:26:12 +0100

    middleware

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,21 @@
 const express = require('express');
 
 const app =  express();
+
+app.use((req, res, next) => {
+    console.log('Requete reçue !');
+    next();
+});
+
+app.use((req, res, next) => {
+    res.stauts(201);
+    next();
+});
+
 app.use((req, res) => {
-    res.json({message: "Votre requete à été bien reçu !"})
+    res.json({message: "Votre requete à été bien reçu !"});
+});
+app.use((req, res , next) => {
+    console.log('Réponse envoyée avec succès !');
 });
 module.exports = app;
