36713100d0f099db0647b8327afc3461abf07779
Eurin-lumen <eurindelagoldcoast@gmail.com>
2022-11-16 00:17:59 +0100

    Exécution

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,7 @@
 const express = require('express');
 
 const app =  express();
+app.use((req, res) => {
+    res.json({message: "Votre requete à été bien reçu !"})
+});
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const http = require('http');
-const server = http.createServer((req, res) => {
-    res.end('Voila la reponse du  premier serveur');
-});
+const app = require('./app');
+
+app.set('port', process.env.PORT || 3000);
+const server = http.createServer(app);
 
 server.listen(process.env.PORT || 3000);
