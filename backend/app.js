571f677dd0f089ed3a22c190f2788a879e8b1962
Eurin-lumen <eurindelagoldcoast@gmail.com>
2022-11-25 02:24:55 +0100

    Lecture et Ecriture

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,8 @@ const Thing = require('./models/Thing');
 
 
 mongoose.connect('mongodb+srv://eurinhash:EHhackroot12@cluster0.npx2mbe.mongodb.net/?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
+  { 
+    useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
@@ -28,26 +29,17 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
+app.get('/api/stuff/:id', (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(404).json({ error }));
+});
 
 app.get('/api/stuff', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
+  Thing.find()
+    .then(thing => res.status(200).json(things))
+    .catch(error => res.status(400).json({error}));
+ 
   });
+
 module.exports = app;
